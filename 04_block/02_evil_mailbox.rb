57ca3c307e3849d1b278580f6cb1e7e73ca673cb
mpg-shu-suzuki <shu.suzuki@medpeer.co.jp>
2023-04-12 12:59:53 +0900

    04_block_02

--- 04_block/02_evil_mailbox.rb
+++ 04_block/02_evil_mailbox.rb
@@ -17,3 +17,46 @@
 # 邪悪な機能
 # 1. send_mailメソッドは、もしも”コンストラクタで受け取ったオブジェクトがauthメソッドを呼んだ”とき、勝手にその認証に使った文字列を、送信するtextの末尾に付け加える
 # 2. つまり、コンストラクタが第2引数に文字列を受け取った時、その文字列はオブジェクト内に保存されないが、send_mailを呼び出したときにこっそりと勝手に送信される
+
+class EvilMailbox
+  def initialize(mailbox, secret = nil)
+    @mailbox = mailbox
+    @mailbox.auth(secret) if secret
+
+    define_singleton_method :send_mail do |recipient, text, &block|
+      result = @mailbox.send_mail(recipient, text + secret.to_s)
+      block.call(result) if block
+      nil
+    end
+  end
+
+  def receive_mail
+    @mailbox.receive_mail
+  end
+end
+
+
+
+#↓テスト通らないver. 上のやつでいけた。
+# class EvilMailbox
+#   def initialize(mailbox, secret = nil)
+#     @mailbox = mailbox
+    
+#     unless secret.nil?
+#       @mailbox.auth(secret)
+#       @secret = secret
+#     end
+#   end
+
+#   def send_mail(recipient, text)
+#     text += @secret unless @secret.nil?
+
+#     result = @mailbox.send_mail(recipient, text)
+#     yield(result) if block_given?
+#     nil
+#   end
+
+#   def receive_mail
+#     @mailbox.receive_mail
+#   end
+# end
